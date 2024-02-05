386cc9a8496089778a24a198bfcd1c48308a28f2
Brett <brett_neighbors@yahoo.com>
2024-02-04 23:41:56 -0700

    Added photo from jsonplaceholder to only display while message is truthy on Brett.jsx

--- src/Components/Brett.jsx
+++ src/Components/Brett.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Brett({ handleBrettClick, message, age }) {
+    const [photo, setPhoto] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function loadPhoto() {
+            setIsLoading(true);
+            const response = await fetch("https://jsonplaceholder.typicode.com/albums/1/photos");
+            const photofromAPI = await response.json();
+            setPhoto(photofromAPI[0]);
+            setIsLoading(false);
+        }
+
+        loadPhoto();
+    }, []);
+
     return (
         <>
             <div className="button">
@@ -8,7 +23,12 @@ function Brett({ handleBrettClick, message, age }) {
                     {message ? "Here is Brett's age (Reclick to hide)" : "Brett"}
                 </button>
 
-                {message && <p>Brett is {age} years old.</p>}
+                {message && (
+                    <>
+                        <p>Brett is {age} years old.</p>
+                        {!isLoading && <img src={photo.url} alt={photo.title} />}
+                    </>
+                )}
             </div>
         </>
     );
