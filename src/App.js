873b3b3d5b5f300d5af4a3ccf724816e5cf2f28c
Brett <brett_neighbors@yahoo.com>
2024-02-24 23:09:44 -0700

    Wrapped Shalaina and Brett components in same element

--- src/App.js
+++ src/App.js
@@ -24,22 +24,23 @@ function App() {
   });
 
   // handle click event that sets message state to true when clicked
+  // Creates new message starting w/ ...message
   const handlePersonClick = (component) => {
     setMessage({ ...message, [component]: !message[component] });
     if (message[component]) {
       setAge({ ...age, [component]: age[component] + 1 });
     }
 
-  const handleAgeClick = {
+    const handleAgeClick = {
 
-  } 
-// Wrapping everything we return within a <Router> component
-// Once you have a <Router> in place, you can use <Route> components to 
-// render or hide components based on the URL path.
+    }
+    // Wrapping everything we return within a <Router> component
+    // Once you have a <Router> in place, you can use <Route> components to 
+    // render or hide components based on the URL path.
 
-// Wrapping a component with <Route> is similar to wrapping it in an if 
-// statement. If the URL matches the path property of the route, the component will 
-// be rendered.
+    // Wrapping a component with <Route> is similar to wrapping it in an if 
+    // statement. If the URL matches the path property of the route, the component will 
+    // be rendered.
 
   };
   return (
@@ -47,21 +48,23 @@ function App() {
       <Routes>
         <Route element={<Header />} />
         <Route exact={true} path="/" element={<Home />} />
-        <Route path="/family-ages">
-          <Route index element={<Brett
-            age={age.brett}
-            message={message.brett}
-            handleBrettClick={() => handlePersonClick("brett")}
-          />} />
-
-          <Route element={<Shalaina
-            age={age.shalaina}
-            message={message.shalaina}
-            handleShalainaClick={() => handlePersonClick("shalaina")}
-          />} />
+        <Route path="/family-ages" element={
+          <div>
+            <Brett
+              age={age.brett}
+              message={message.brett}
+              handleBrettClick={() => handlePersonClick("brett")}
+            />
+            <Shalaina
+              age={age.shalaina}
+              message={message.shalaina}
+              handleShalainaClick={() => handlePersonClick("shalaina")}
+            />
+          </div>
+        }>
         </Route>
-        
-        
+
+
         {/*
           
         
@@ -76,7 +79,7 @@ function App() {
           handleEverleeClick={() => handleClick("everlee")}
         />
         */}
-        
+
       </Routes>
 
     </Router>
