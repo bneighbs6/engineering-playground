8ac4252083c03995dd602f26d63bfb599c2a700d
Brett <brett_neighbors@yahoo.com>
2024-03-20 19:55:43 -0600

    Updated onClicks for each state in FamilyLocations

--- src/Components/web-pages/FamilyLocations.jsx
+++ src/Components/web-pages/FamilyLocations.jsx
@@ -7,10 +7,10 @@ function FamilyLocations({ handleStateClick }) {
         <div className="family-locations">
             <header className="text-center m-4">Where do they live?</header>
             <ul>
-                <li><button className="btn btn-primary" onClick={() => {return "False"}}>Washington</button></li>
-                <li><button className="btn btn-warning">Nevada</button></li>
-                <li><button className="btn btn-outline-danger">Montana</button></li>
-                <li><button className="btn btn-outline-warning">Texas</button></li>
+                <li><button className="btn btn-success my-4" onClick={() => {console.log('Need to display false on page')}}>Washington</button></li>
+                <li><button className="btn btn-primary my-4" onClick={() => {console.log("Need to display false on page")}}>Nevada</button></li>
+                <li><button className="btn btn-danger my-4" onClick={() => {console.log("Need to display true on page")}}>Montana</button></li>
+                <li><button className="btn btn-warning my-4" onClick={() => {console.log("Need to display false on page")}}>Texas</button></li>
             </ul>
         </div>
     )
